2f7e96d8190d3ec41ccd26f2f85d92dd60445641
abuzain153 <abuzain186@gmail.com>
2025-05-16 08:51:49 +0300

    Update wsgi.py

--- gym_tracker/wsgi.py
+++ gym_tracker/wsgi.py
@@ -1,9 +1,9 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling  # استيراد Cling و MediaCling
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gym_tracker.settings')
 
 application = Cling(MediaCling(get_wsgi_application()))
+
 
