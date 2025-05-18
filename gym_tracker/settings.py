2ffbb70724b9f0bc5aaaf928a96f6c33b237567f
abuzain153 <abuzain186@gmail.com>
2025-05-18 08:18:21 +0300

    Update settings.py

--- gym_tracker/settings.py
+++ gym_tracker/settings.py
@@ -59,14 +59,17 @@ TEMPLATES = [
 ]
 
 
+# ========== DATABASES ==========
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('RENDER_DB_NAME'),
+        'USER': os.environ.get('RENDER_DB_USER'),
+        'PASSWORD': os.environ.get('RENDER_DB_PASSWORD'),
+        'HOST': os.environ.get('RENDER_DB_HOST'),
+        'PORT': os.environ.get('RENDER_DB_PORT', 5432),
     }
 }
-
-
 
 
 # ========== PASSWORD VALIDATORS ==========
